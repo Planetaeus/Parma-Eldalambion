65502722a4f241de98aa94fcb8875c5d5f7cf43a
Planetaeus <galinndan.dawnfire@gmail.com>
2017-03-16 10:14:30 -0400

    I tried and failed to customize the scrollbar.
    I will try again tomorrow.

--- ElvishEducation/public_html/js/index.js
+++ ElvishEducation/public_html/js/index.js
@@ -3,5 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+$( 'div.banner' ).hide();
+$( '.button' ).hide();
 
+function main() {
+    $( 'div.banner' ).fadeIn( 600 );
+    $( '.button' ).fadeIn( 1000 );
+}
 
+$(document).ready(main);
